d42f748e671dd25403d4bd1367c2967e556d9b32
bastien17-dev <bastien.paquier@gmail.com>
2020-05-13 13:02:31 +0200

    add backgroung image and content

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,79 +1,139 @@
-const img1 = require('../static/img/main_home.jpg');
-const img2 = require('../static/img/main_spots.jpg');
-const img3 = require('../static/img/main_news.jpg');
+document.addEventListener('DOMContentLoaded', () => {
+  const img1 = require('../static/img/main_home.jpg');
+  const img2 = require('../static/img/main_spots.jpg');
+  const img3 = require('../static/img/main_news.jpg');
 
-// Model qui permet de récupérer la data
-class Model {
-  constructor() {
-    this.data = [
-      {
-        url: '/',
-        documentTitle: 'Homepage',
-        imgSrc: img1,
-        title: 'Acceuil',
-      },
-      {
-        url: '/spots',
-        documentTitle: 'All spots',
-        imgSrc: img2,
-        title: 'Spots map',
-      },
-      {
-        url: '/news',
-        documentTitle: 'News',
-        imgSrc: img3,
-        title: 'News',
-      },
-    ];
+  // Model qui permet de récupérer la data
+  class Model {
+    constructor() {
+      this.pages = [
+        {
+          url: '#',
+          documentTitle: 'Homepage',
+          imgSrc: img1,
+          title: 'Acceuil',
+          content: `
+            <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate suscipit nihil dolores repellendus alias illum vitae. Et unde enim ducimus, exercitationem dolorum, eum omnis autem, quasi ipsa a illum animi.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate suscipit nihil dolores repellendus alias illum vitae. Et unde enim ducimus, exercitationem dolorum, eum omnis autem, quasi ipsa a illum animi.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate suscipit nihil dolores repellendus alias illum vitae. Et unde enim ducimus, exercitationem dolorum, eum omnis autem, quasi ipsa a illum animi.
+            </div>
+          `,
+        },
+        {
+          url: '#spots',
+          documentTitle: 'All spots',
+          imgSrc: img2,
+          title: 'Spots map',
+        },
+        {
+          url: '#news',
+          documentTitle: 'News',
+          imgSrc: img3,
+          title: 'News',
+        },
+      ];
+    }
+
+    getPageByUrl(url) {
+      return this.pages.find((page) => page.url === url);
+    }
   }
 
-  getPageByUrl(url) {
-    return this.data.find((page) => page.url === url);
+  // View qui permet de modifier le template html
+  class View {
+    constructor(pages) {
+      this.container = document.querySelector('.container');
+      this.container.innerHTML = '';
+      this.addHeader(pages);
+    }
+
+    addHeader(pages) {
+      const fragment = document.createDocumentFragment();
+
+      const $header = document.createElement('header');
+      $header.classList.add('header');
+
+      const $ulElt = document.createElement('ul');
+      $ulElt.classList.add('header__nav');
+
+      const $title = document.createElement('p');
+      $title.classList.add('header__title');
+      $title.innerText = 'Worlwide surf spots database';
+
+      pages.forEach((page) => {
+        const $liElt = document.createElement('li');
+        const $button = document.createElement('div');
+        $button.innerText = page.title;
+        $button.addEventListener('click', () => {
+          location.hash = page.url;
+        });
+
+        $liElt.appendChild($button);
+        $ulElt.appendChild($liElt);
+      });
+
+      $header.appendChild($ulElt);
+      $header.appendChild($title);
+      fragment.appendChild($header);
+      this.container.appendChild(fragment);
+    }
+
+    addContent(content) {
+      const fragment = document.createDocumentFragment();
+      const $content = document.createElement('div');
+      $content.classList.add('container__content');
+      $content.innerHTML = content;
+
+      fragment.appendChild($content);
+      this.container.appendChild(fragment);
+    }
+    changedocumentTile(option) {
+      document.title = option;
+    }
+
+    setBackgroungImage(option) {
+      const fragment = document.createDocumentFragment();
+
+      const $imageContainer = document.createElement('div');
+      $imageContainer.classList.add('container__image');
+      $imageContainer.style.backgroundImage = `url(${option})`;
+
+      fragment.appendChild($imageContainer);
+      this.container.appendChild(fragment);
+    }
+
+    setMainTitle(option) {
+      const fragment = document.createDocumentFragment();
+
+      const $mainTitle = document.createElement('h1');
+      $mainTitle.classList.add('container__title');
+      $mainTitle.textContent = option;
+
+      fragment.appendChild($mainTitle);
+      this.container.appendChild(fragment);
+    }
   }
-}
 
-// View qui permet de modifier le template html
-class View {
-  constructor() {
-    this.container = document.querySelector('.container');
+  // Controller qui permet de gérer l'ensemble de l'application
+  function controller() {
+    const data = new Model();
+
+    const currentPage = data.getPageByUrl(location.hash || '#');
+
+    const render = new View(data.pages);
+    render.changedocumentTile(currentPage.documentTitle);
+    render.setBackgroungImage(currentPage.imgSrc);
+    render.setMainTitle(currentPage.title);
+
+    if (currentPage.content) {
+      render.addContent(currentPage.content);
+    }
   }
 
-  changedocumentTile(option) {
-    document.title = option;
-  }
+  window.addEventListener('hashchange', () => {
+    controller();
+  });
 
-  setBackgroungImage(option) {
-    const $imageContainer = document.createElement('div');
-    $imageContainer.classList.add('container__image');
-
-    const $backgroundImg = document.createElement('img');
-    $backgroundImg.setAttribute('src', option);
-    $backgroundImg.classList.add('image');
-
-    $imageContainer.appendChild($backgroundImg);
-    this.container.appendChild($imageContainer);
-  }
-
-  setMainTitle(option) {
-    const $mainTitle = document.createElement('h1');
-    $mainTitle.classList.add('container__title');
-    $mainTitle.textContent = option;
-
-    this.container.appendChild($mainTitle);
-  }
-}
-
-// Controller qui permet de gérer l'ensemble de l'application
-function controller() {
-  const currentUrl = document.location.pathname;
-  const data = new Model();
-
-  const currentPage = data.getPageByUrl(currentUrl);
-
-  const render = new View();
-  render.changedocumentTile(currentPage.documentTitle);
-  render.setBackgroungImage(currentPage.imgSrc);
-  render.setMainTitle(currentPage.title);
-}
-
-controller();
+  controller();
+});
